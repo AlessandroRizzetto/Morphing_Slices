bfbe72682b3ff92369763412729edeb1fcc1fe8e
Alessandro Rizzetto <93980580+elrich2610@users.noreply.github.com>
2022-08-21 21:44:48 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 import os
 
-topology=input("Topology? ")
+topology=input("Insert the selected topology: ")
 if topology in ["star","star2","tree","linear","ring"]:
     os.system("gnome-terminal -e 'bash -c \"./start.sh " + topology + "; exec bash\"'")
     os.system("gnome-terminal -e 'bash -c \"sudo python3 baseTopology.py; exec bash\"'")
